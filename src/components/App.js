089d347329dbc28520a63eb63f256901923f6390
dhanvantichelani <dhanvantichelani865@gmail.com>
2023-08-17 11:46:46 +0530

    form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  // const[name,setName] = useState();
+  // const[email,setEmail] = useState();
+  const[collectData,setCollectData]=useState({name:"",email:"",password:""});
+  
+  const DisplayDetails =(event)=>{
+    event.preventDefault();
+    console.log(collectData)
+    
+}
+const changeData =(event)=>{
+  const {name,value} = event.target
+  setCollectData((oldData)=>{
+    return {...oldData,[name]:value}
+
+  })
+}
+  
   return (
     <div id="main">
-      <p>I am learning React. My life is getting better.</p>
+      <form onSubmit={DisplayDetails}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value={collectData.name} onChange={changeData}  />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={collectData.email} onChange={changeData} />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" value={collectData.password} onChange={changeData} /><br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
